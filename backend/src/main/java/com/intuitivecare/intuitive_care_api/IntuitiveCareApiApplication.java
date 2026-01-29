fd6f513d7dffeb2493f0e1ae11e84e2f2c3a595a
Hiago <figueiredoh788@gmail.com>
2026-01-29 09:00:00 -0400

    Estrutura do Projeto

---
+++ backend/src/main/java/com/intuitivecare/intuitive_care_api/IntuitiveCareApiApplication.java
@@ -0,0 +1,14 @@
+package com.intuitivecare.intuitive_care_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntuitiveCareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntuitiveCareApiApplication.class, args);
+		System.out.println("esta funcionando");
+	}
+
+}
---
+++ backend/src/test/java/com/intuitivecare/intuitive_care_api/IntuitiveCareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuitivecare.intuitive_care_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntuitiveCareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
